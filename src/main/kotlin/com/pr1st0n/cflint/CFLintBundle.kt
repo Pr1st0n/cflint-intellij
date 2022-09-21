1856c5913e6599034dc1c665888a63e88f91c3c0
Pr1st0n <k0t9p@yahoo.com>
2022-09-21 10:23:00 +0200

    Refactor CFLintConfigurationService usage, adjust tests

--- src/main/kotlin/com/pr1st0n/cflint/CFLintBundle.kt
+++ src/main/kotlin/com/pr1st0n/cflint/CFLintBundle.kt
@@ -1,21 +1,16 @@
 package com.pr1st0n.cflint
 
-import com.intellij.DynamicBundle
+import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
 private const val BUNDLE = "messages.CFLintBundle"
 
-object CFLintBundle : DynamicBundle(BUNDLE) {
+object CFLintBundle : AbstractBundle(BUNDLE) {
 
     @Suppress("SpreadOperator")
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
         getMessage(key, *params)
-
-    @Suppress("SpreadOperator", "unused")
-    @JvmStatic
-    fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
-        getLazyMessage(key, *params)
 }
--- src/main/kotlin/com/pr1st0n/cflint/CFLintConfigurationService.kt
+++ src/main/kotlin/com/pr1st0n/cflint/CFLintConfigurationService.kt
@@ -7,21 +7,14 @@ import com.cflint.config.ConfigUtils
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.project.Project
 import java.io.File
 
 @State(name = "CFLintConfiguration")
-class CFLintConfiguration : PersistentStateComponent<CFLintState> {
+class CFLintConfigurationService : PersistentStateComponent<CFLintState> {
     private val logger = Logger.getInstance("CFLint.CFLintInspection")
     private var config = CFLintConfig()
     private var initialized = false
     private var myState = CFLintState()
-
-    companion object {
-        fun getInstance(project: Project): com.pr1st0n.cflint.CFLintConfiguration {
-            return project.getService(com.pr1st0n.cflint.CFLintConfiguration::class.java)
-        }
-    }
 
     private fun getConfig(basePath: String?): CFLintConfiguration {
         if (!this.initialized && !setCustomRules() && !setBaseConfig(basePath)) {
